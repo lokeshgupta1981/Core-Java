77898c74349dfe9fcaee7a4912f2a63a75a0c753
Lokesh Gupta <howtodoinjava@gmail.com>
2020-05-03 22:41:11 +0530

    Java 8 DayOfWeek Example

---
+++ src/com/howtodoinjava/core/datetime/DayOfWeekExample.java
@@ -0,0 +1,33 @@
+package com.howtodoinjava.core.datetime;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class DayOfWeekExample {
+	public static void main(String[] args) 
+	{
+		LocalDate today = LocalDate.now();
+		
+		System.out.println( today.getDayOfWeek() );
+		System.out.println( today.getDayOfWeek().getValue() );
+		
+		String textValue = getDayString(today, Locale.getDefault());
+		
+		System.out.println(textValue);
+		
+		textValue = getDayString(today, Locale.GERMAN);
+		
+		System.out.println(textValue);
+		
+		ZonedDateTime.now().getDayOfWeek();
+	}
+	
+	public static String getDayString(LocalDate date, Locale locale) {
+	    DayOfWeek day = date.getDayOfWeek();
+	    return day.getDisplayName(TextStyle.FULL, locale);
+	}
+}
--- src/com/howtodoinjava/core/datetime/README.md
+++ src/com/howtodoinjava/core/datetime/README.md
@@ -3,3 +3,4 @@
 1. [Java Date and Time Tutorial](https://howtodoinjava.com/java-date-and-time-apis/)
 2. [Java – Get Next and Previous Date](https://howtodoinjava.com/java/date-time/java8-next-previous-date/)
 3. [Java – TemporalQuery](https://howtodoinjava.com/java/date-time/temporalquery/)
+4. [Java 8 – DayOfWeek](https://howtodoinjava.com/java/date-time/find-dayofweek/)
