9a0c3eba6ffaa5874f7a2326b2e9c07975b4b2b8
Lokesh Gupta <howtodoinjava@gmail.com>
2020-05-05 18:27:23 +0530

    Update Read me

--- src/com/howtodoinjava/core/datetime/BusinessDaysExamples.java
+++ src/com/howtodoinjava/core/datetime/BusinessDaysExamples.java
@@ -1,100 +1,92 @@
 package com.howtodoinjava.core.datetime;
 
-	import java.time.DayOfWeek;
-	import java.time.LocalDate;
-	import java.time.temporal.ChronoUnit;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
-	import java.util.Optional;
-	import java.util.function.Predicate;
-	import java.util.stream.Stream;
-	
-	public class BusinessDaysExamples 
-	{
-		public static void main(String[] args) 
-		{
-			LocalDate today = LocalDate.of(2020, 5, 5);
-			
-			System.out.println(addBusinessDays(today, 8, Optional.empty()));
-			
-			//Add one holiday for testing
-			List<LocalDate> holidays = new ArrayList<>();
-			holidays.add(LocalDate.of(2020, 5, 11));
-			holidays.add(LocalDate.of(2020, 5, 1));
-			
-			System.out.println(addBusinessDays(today, 8, Optional.of(holidays)));
-			
-			System.out.println(subtractBusinessDays(today, 8, Optional.empty()));
-			System.out.println(subtractBusinessDays(today, 8, Optional.of(holidays)));
-			
-			System.out.println(countBusinessDaysBetween(today, today.plusDays(20), Optional.empty()));
-			System.out.println(countBusinessDaysBetween(today, today.plusDays(20), Optional.of(holidays)));
-		}
-		
-		private static LocalDate addBusinessDays(LocalDate localDate, int days, Optional<List<LocalDate>> holidays)
-		{
-			if(localDate == null || days <= 0 || holidays == null)
-			{
-				throw new IllegalArgumentException("Invalid method argument(s) "
-						+ "to addBusinessDays("+localDate+","+days+","+holidays+")");
-			}
-			
-			Predicate<LocalDate> isHoliday = date -> holidays.isPresent() ? holidays.get().contains(date) : false;
-			
-			Predicate<LocalDate> isWeekend = date -> date.getDayOfWeek() == DayOfWeek.SATURDAY 
-													|| date.getDayOfWeek() == DayOfWeek.SUNDAY;
-			
-			LocalDate result = localDate;
-			while (days > 0) {
-		        result = result.plusDays(1);
-		        if (isHoliday.or(isWeekend).negate().test(result)) {
-		            days--;
-		        }
-		    }
-			return result;
-		}
-		
-		private static LocalDate subtractBusinessDays(LocalDate localDate, int days, Optional<List<LocalDate>> holidays)
-		{
-			if(localDate == null || days <= 0 || holidays == null)
-			{
-				throw new IllegalArgumentException("Invalid method argument(s) "
-						+ "to subtractBusinessDays("+localDate+","+days+","+holidays+")");
-			}
-			
-			Predicate<LocalDate> isHoliday = date -> holidays.isPresent() ? holidays.get().contains(date) : false;
-			
-			Predicate<LocalDate> isWeekend = date -> date.getDayOfWeek() == DayOfWeek.SATURDAY 
-													|| date.getDayOfWeek() == DayOfWeek.SUNDAY;
-			
-			LocalDate result = localDate;
-			while (days >= 0) {
-		        result = result.minusDays(1);
-		        if (isHoliday.or(isWeekend).negate().test(result)) {
-		            days--;
-		        }
-		    }
-			return result;
-		}
-		
-		private static long countBusinessDaysBetween(LocalDate startDate, LocalDate endDate, Optional<List<LocalDate>> holidays)
-		{
-			if(startDate == null || endDate == null || holidays == null)
-			{
-				throw new IllegalArgumentException("Invalid method argument(s) to countBusinessDaysBetween("+startDate+","+endDate+","+holidays+")");
-			}
-			
-			Predicate<LocalDate> isHoliday = date -> holidays.isPresent() ? holidays.get().contains(date) : false;
-			
-			Predicate<LocalDate> isWeekend = date -> date.getDayOfWeek() == DayOfWeek.SATURDAY 
-													|| date.getDayOfWeek() == DayOfWeek.SUNDAY;
-			
-			long daysBetween = ChronoUnit.DAYS.between(startDate, endDate);
-			
-			long businessDays = Stream.iterate(startDate, date -> date.plusDays(1))
-				    					.limit(daysBetween)
-				    					.filter(isHoliday.or(isWeekend).negate())
-				    					.count();
-			return businessDays;
-		}
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class BusinessDaysExamples {
+	public static void main(String[] args) {
+		LocalDate today = LocalDate.of(2020, 5, 5);
+
+		System.out.println(addBusinessDays(today, 8, Optional.empty()));
+
+		// Add one holiday for testing
+		List<LocalDate> holidays = new ArrayList<>();
+		holidays.add(LocalDate.of(2020, 5, 11));
+		holidays.add(LocalDate.of(2020, 5, 1));
+
+		System.out.println(addBusinessDays(today, 8, Optional.of(holidays)));
+
+		System.out.println(subtractBusinessDays(today, 8, Optional.empty()));
+		System.out.println(subtractBusinessDays(today, 8, Optional.of(holidays)));
+
+		System.out.println(countBusinessDaysBetween(today, today.plusDays(20), Optional.empty()));
+		System.out.println(countBusinessDaysBetween(today, today.plusDays(20), Optional.of(holidays)));
 	}
+
+	private static LocalDate addBusinessDays(LocalDate localDate, int days, Optional<List<LocalDate>> holidays) {
+		if (localDate == null || days <= 0 || holidays == null) {
+			throw new IllegalArgumentException("Invalid method argument(s) " + "to addBusinessDays(" + localDate + ","
+					+ days + "," + holidays + ")");
+		}
+
+		Predicate<LocalDate> isHoliday = date -> holidays.isPresent() ? holidays.get().contains(date) : false;
+
+		Predicate<LocalDate> isWeekend = date -> date.getDayOfWeek() == DayOfWeek.SATURDAY
+				|| date.getDayOfWeek() == DayOfWeek.SUNDAY;
+
+		LocalDate result = localDate;
+		while (days > 0) {
+			result = result.plusDays(1);
+			if (isHoliday.or(isWeekend).negate().test(result)) {
+				days--;
+			}
+		}
+		return result;
+	}
+
+	private static LocalDate subtractBusinessDays(LocalDate localDate, int days, Optional<List<LocalDate>> holidays) {
+		if (localDate == null || days <= 0 || holidays == null) {
+			throw new IllegalArgumentException("Invalid method argument(s) " + "to subtractBusinessDays(" + localDate
+					+ "," + days + "," + holidays + ")");
+		}
+
+		Predicate<LocalDate> isHoliday = date -> holidays.isPresent() ? holidays.get().contains(date) : false;
+
+		Predicate<LocalDate> isWeekend = date -> date.getDayOfWeek() == DayOfWeek.SATURDAY
+				|| date.getDayOfWeek() == DayOfWeek.SUNDAY;
+
+		LocalDate result = localDate;
+		while (days > 0) {
+			result = result.minusDays(1);
+			if (isHoliday.or(isWeekend).negate().test(result)) {
+				days--;
+			}
+		}
+		return result;
+	}
+
+	private static long countBusinessDaysBetween(LocalDate startDate, LocalDate endDate,
+			Optional<List<LocalDate>> holidays) {
+		if (startDate == null || endDate == null || holidays == null) {
+			throw new IllegalArgumentException("Invalid method argument(s) to countBusinessDaysBetween(" + startDate
+					+ "," + endDate + "," + holidays + ")");
+		}
+
+		Predicate<LocalDate> isHoliday = date -> holidays.isPresent() ? holidays.get().contains(date) : false;
+
+		Predicate<LocalDate> isWeekend = date -> date.getDayOfWeek() == DayOfWeek.SATURDAY
+				|| date.getDayOfWeek() == DayOfWeek.SUNDAY;
+
+		long daysBetween = ChronoUnit.DAYS.between(startDate, endDate);
+
+		long businessDays = Stream.iterate(startDate, date -> date.plusDays(1)).limit(daysBetween)
+				.filter(isHoliday.or(isWeekend).negate()).count();
+		return businessDays;
+	}
+}
--- src/com/howtodoinjava/core/datetime/README.md
+++ src/com/howtodoinjava/core/datetime/README.md
@@ -6,4 +6,5 @@
 4. [Java 8 – DayOfWeek](https://howtodoinjava.com/java/date-time/find-dayofweek/)
 5. [Java 8 – Date Comparison](https://howtodoinjava.com/java/date-time/compare-dates/)
 6. [Java 8 – Current Date and Time](https://howtodoinjava.com/java/date-time/current-date-time/)
-7. [Java 8 – Add or subtract N business days](https://howtodoinjava.com/java/date-time/add-subtract-business-days/)   
+7. [Java 8 – Add or subtract N business days](https://howtodoinjava.com/java/date-time/add-subtract-business-days/)  
+8. [Java 8 - Calculate business days between two dates](https://howtodoinjava.com/java/date-time/calculate-business-days/) 
