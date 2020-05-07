6758aa67a0c698bb3cf7f8e62b181795402d4d4d
Lokesh Gupta <howtodoinjava@gmail.com>
2020-05-07 23:37:15 +0530

    Get All Dates Between Two Dates

---
+++ src/com/howtodoinjava/core/datetime/GetAllDatesBetweenTwoDates.java
@@ -0,0 +1,69 @@
+package com.howtodoinjava.core.datetime;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+public class GetAllDatesBetweenTwoDates 
+{
+	public static void main(String[] args) 
+	{
+		LocalDate startDate = LocalDate.now();
+		LocalDate endDate = startDate.plusMonths(2);
+		
+		// Java 9
+		
+		List<LocalDate> listOfDates = startDate.datesUntil(endDate)
+											.collect(Collectors.toList());
+		
+		System.out.println(listOfDates.size());
+		
+		// Java 8 - Method 1
+		
+		long numOfDays = ChronoUnit.DAYS.between(startDate, endDate);
+		 
+		List<LocalDate> listOfDates1 = Stream.iterate(startDate, date -> date.plusDays(1))
+										 	.limit(numOfDays)
+										 	.collect(Collectors.toList());
+		
+		System.out.println(listOfDates1.size());
+		
+		// Java 8 - Method 2
+		 
+		List<LocalDate> listOfDates2 = LongStream.range(0, numOfDays)
+											.mapToObj(startDate::plusDays)
+										 	.collect(Collectors.toList());
+		
+		System.out.println(listOfDates2.size());
+		
+		// Java 7
+		Date todaysDate = new Date();
+		Date anotherDate = new Date(todaysDate.getTime() + 61*24*60*60*1000l);
+		
+		List<Date> listOfDates3 = getDaysBetweenDates(todaysDate, anotherDate);
+		
+		System.out.println(listOfDates3.size());
+	}
+	
+	public static List<Date> getDaysBetweenDates(Date startdate, Date enddate)
+	{
+	    List<Date> dates = new ArrayList<Date>();
+	    Calendar calendar = new GregorianCalendar();
+	    calendar.setTime(startdate);
+
+	    while (calendar.getTime().before(enddate))
+	    {
+	        Date result = calendar.getTime();
+	        dates.add(result);
+	        calendar.add(Calendar.DATE, 1);
+	    }
+	    return dates;
+	}
+}
--- src/com/howtodoinjava/core/datetime/README.md
+++ src/com/howtodoinjava/core/datetime/README.md
@@ -1,11 +1,12 @@
 # Related Tutorials
 
 1. [Java Date and Time Tutorial](https://howtodoinjava.com/java-date-and-time-apis/)
-2. [Java 8 – Get Next and Previous Date](https://howtodoinjava.com/java/date-time/java8-next-previous-date/)
-3. [Java 8 – TemporalQuery](https://howtodoinjava.com/java/date-time/temporalquery/)
-4. [Java 8 – DayOfWeek](https://howtodoinjava.com/java/date-time/find-dayofweek/)
-5. [Java 8 – Date Comparison](https://howtodoinjava.com/java/date-time/compare-dates/)
-6. [Java 8 – Current Date and Time](https://howtodoinjava.com/java/date-time/current-date-time/)
-7. [Java 8 – Add or subtract N business days](https://howtodoinjava.com/java/date-time/add-subtract-business-days/)  
+2. [Java 8 - Get Next and Previous Date](https://howtodoinjava.com/java/date-time/java8-next-previous-date/)
+3. [Java 8 - TemporalQuery](https://howtodoinjava.com/java/date-time/temporalquery/)
+4. [Java 8 - DayOfWeek](https://howtodoinjava.com/java/date-time/find-dayofweek/)
+5. [Java 8 - Date Comparison](https://howtodoinjava.com/java/date-time/compare-dates/)
+6. [Java 8 - Current Date and Time](https://howtodoinjava.com/java/date-time/current-date-time/)
+7. [Java 8 - Add or subtract N business days](https://howtodoinjava.com/java/date-time/add-subtract-business-days/)  
 8. [Java 8 - Calculate business days between two dates](https://howtodoinjava.com/java/date-time/calculate-business-days/) 
 9. [Java 8 - Calculate days between two dates](https://howtodoinjava.com/java/date-time/calculate-days-between-dates/)
+10. [Java â€“ Get All Dates Between Two Dates](https://howtodoinjava.com/java/date-time/dates-between-two-dates/)
