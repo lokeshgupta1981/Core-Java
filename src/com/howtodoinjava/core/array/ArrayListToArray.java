991b9bd2891ac32df822da288620ac7708cbbada
lokeshgupta1981 <howtodoinjava@gmail.com>
2021-07-18 02:11:35 +0530

    Java example

---
+++ src/com/howtodoinjava/core/array/ArrayListToArray.java
@@ -0,0 +1,23 @@
+package com.howtodoinjava.core.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListToArray 
+{
+	public static void main(final String[] args) {
+		ArrayList<Integer> listOfIntegers = new ArrayList<>();
+		listOfIntegers.add(1);
+		listOfIntegers.add(2);
+		listOfIntegers.add(3);
+		
+		System.out.println(listOfIntegers);
+		
+		int[] intArray = listOfIntegers 
+					.stream()
+					.mapToInt(Integer::intValue)
+					.toArray();
+		
+		System.out.println(Arrays.toString(intArray));
+	}
+}
