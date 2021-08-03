ac5b0828c1a784ad9201e11db4d8a26147716ca6
lokeshgupta1981 <howtodoinjava@gmail.com>
2021-08-04 00:08:09 +0530

    Code updates

--- src/com/howtodoinjava/core/datetime/CheckWeekendJava8.java
+++ src/com/howtodoinjava/core/datetime/CheckWeekendJava8.java
@@ -4,20 +4,17 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.ChronoField;
 
-public class CheckWeekend_Java8 {
+public class CheckWeekendJava8 {
     public static void main(final String[] args) {
         LocalDate today = LocalDate.now();
         System.out.println("Is weekend : " + isWeekend(today));
-        
-        LocalDate someDate = LocalDate.of(2021, 1, 2); //2nd-Jan-2021
+
+        LocalDate someDate = LocalDate.of(2021, 1, 2); // 2nd-Jan-2021
         System.out.println("Is weekend : " + isWeekend(someDate));
     }
 
     public static boolean isWeekend(final LocalDate ld) {
         DayOfWeek day = DayOfWeek.of(ld.get(ChronoField.DAY_OF_WEEK));
-        if (day == DayOfWeek.SUNDAY || day == DayOfWeek.SATURDAY) {
-            return true;
-        }
-        return false;
+        return day == DayOfWeek.SUNDAY || day == DayOfWeek.SATURDAY;
     }
 }
