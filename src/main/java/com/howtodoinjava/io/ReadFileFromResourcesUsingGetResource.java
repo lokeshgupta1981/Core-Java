eded78fce5c8389e45867154d56726c46e14c9a0
lokeshgupta1981 <lokeshgupta1981@gmail.com>
2021-10-27 00:07:01 +0530

    Refactoring

--- src/main/java/com/howtodoinjava/io/ReadFileFromResourcesUsingGetResource.java
+++ src/main/java/com/howtodoinjava/io/ReadFileFromResourcesUsingGetResource.java
@@ -1,10 +1,7 @@
 package com.howtodoinjava.io;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.file.Files;
 
@@ -41,18 +38,5 @@ public class ReadFileFromResourcesUsingGetResource {
     {
         String content = new String(Files.readAllBytes(file.toPath()));
         System.out.println(content);
-    }
-    
-    private void printFileContent(InputStream is) throws IOException 
-    {
-        try (InputStreamReader isr = new InputStreamReader(is); 
-                BufferedReader br = new BufferedReader(isr);) 
-        {
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-            is.close();
-        }
     }
 }
