7607648143b5dd4e87f2844827e41b74b6757bd2
lokeshgupta1981 <lokeshgupta1981@gmail.com>
2021-12-13 00:21:00 +0530

    Added Lombok

--- src/main/java/com/howtodoinjava/core/streams/distinct/DistinctPrimitivesAndStrings.java
+++ src/main/java/com/howtodoinjava/core/streams/distinct/DistinctPrimitivesAndStrings.java
@@ -1,22 +1,18 @@
 package com.howtodoinjava.core.streams.distinct;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class DistinctPrimitivesAndStrings 
-{
-    public static void main(String[] args) 
-    {
-        Collection<String> list = Arrays.asList("A", "B", "C", "D", "A", "B", "C");
+public class DistinctPrimitivesAndStrings {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("A", "B", "C", "D", "A", "B", "C");
 
         // Get collection without duplicate i.e. distinct only
-        List<String> distinctElements = list.stream()
-            .distinct()
-            .collect(Collectors.toList());
+        List<String> distinctItems =
+                list.stream().distinct().collect(Collectors.toList());
 
         // Let's verify distinct elements
-        System.out.println(distinctElements);
+        System.out.println(distinctItems);
     }
 }
