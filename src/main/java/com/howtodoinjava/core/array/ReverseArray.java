ce68d3d0a795b985daac8053ceceb0328bc4d97f
lokeshgupta1981 <lokeshgupta1981@gmail.com>
2022-01-31 17:54:53 +0530

    Create ReverseArray.java

---
+++ src/main/java/com/howtodoinjava/core/array/ReverseArray.java
@@ -0,0 +1,33 @@
+package com.howtodoinjava.core.array;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.IntStream;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        //1
+        String[] array = {"A", "B", "C", "D", "E"};
+        Collections.reverse(Arrays.asList(array));
+        System.out.println(Arrays.toString(array));
+
+        //2
+        array = new String[]{"A","B","C","D","E"};
+        for (int i = 0; i < array.length / 2; i++) {
+            String temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        System.out.println(Arrays.toString(array));
+        
+        final String[] arr = new String[]{"A","B","C","D","E"};
+        Object[] reversedArr = IntStream.rangeClosed(1, array.length)
+            .mapToObj(i -> arr[arr.length - i])
+            .toArray();
+        System.out.println(Arrays.toString(reversedArr));
+        
+        ArrayUtils.reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
