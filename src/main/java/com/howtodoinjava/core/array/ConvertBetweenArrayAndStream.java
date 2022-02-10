e51f80a3ae4aeea0f983f9db640688853e19805b
lokeshgupta1981 <howtodoinjava@gmail.com>
2022-02-11 01:09:26 +0530

    Array and Stream Examples

---
+++ src/main/java/com/howtodoinjava/core/array/ConvertBetweenArrayAndStream.java
@@ -0,0 +1,24 @@
+package com.howtodoinjava.core.array;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+
+public class ConvertBetweenArrayAndStream {
+  @SuppressWarnings({"UnusedAssignment"})
+  public static void main(String[] args) {
+    int[] primitiveArray = {0,1,2,3,4};
+    String[] stringArray = {"a", "b", "c", "d", "e"};
+
+    //1. Array -> Stream
+    Stream<String> strStream = Arrays.stream(stringArray);
+    IntStream intStream = Arrays.stream(primitiveArray);
+    Stream<Integer> integerStream = Arrays.stream(primitiveArray).boxed();
+
+    //2.Stream -> Array
+    stringArray = strStream.toArray(String[]::new);
+    primitiveArray = intStream.toArray();
+    primitiveArray = integerStream.mapToInt(i -> i).toArray();
+  }
+}
