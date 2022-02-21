fe4f82889bfd43ca47c5dfcf4a011f2294e3b125
Lokesh Gupta <lokeshgupta1981@gmail.com>
2022-02-21 15:08:49 +0530

    Update FormatMilliseconds.java

--- src/main/java/com/howtodoinjava/core/datetime/FormatMilliseconds.java
+++ src/main/java/com/howtodoinjava/core/datetime/FormatMilliseconds.java
@@ -6,24 +6,22 @@ import org.apache.commons.lang3.time.DurationFormatUtils;
 
 public class FormatMilliseconds {
   public static void main(final String[] args) {
-    Duration duration = Duration.ofMillis(38114000);
     
-    //1
-    long seconds = duration.getSeconds();
+    long millis = 54321000;
     
-    long HH = seconds / 3600;
-    long MM = seconds % 3600 / 60;
-    long SS = seconds % 60;
+    //Java 8
+    Duration duration = Duration.ofMillis(millis);
+        
+    long h = duration.toHours();
+    long m = duration.toMinutes() % 60;
+    long s = duration.getSeconds() % 60;
     
-    String timeInHHMMSS = String.format("%02d:%02d:%02d", HH, MM, SS);
-    System.out.println(timeInHHMMSS);
+    String timeInHms = String.format("%02d:%02d:%02d", h, m, s);
+    System.out.println(timeInHms);
     
-    //2
-    timeInHHMMSS = DurationFormatUtils.formatDuration(38114000, "HH:MM:SS", true);
-    System.out.println(timeInHHMMSS);
-    
-    //3
-    
+    //Apache Commons
+    timeInHms = DurationFormatUtils.formatDuration(millis, "HH:mm:ss", true);
+    System.out.println(timeInHms);
   }
 }
 
