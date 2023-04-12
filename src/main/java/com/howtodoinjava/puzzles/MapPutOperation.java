b7a6aa62949072ead0e1887e3f0d220570ea6e47
lokeshgupta1981 <howtodoinjava@gmail.com>
2023-04-12 21:36:18 +0530

    Puzzles

---
+++ src/main/java/com/howtodoinjava/puzzles/MapPutOperation.java
@@ -0,0 +1,18 @@
+package com.howtodoinjava.puzzles;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapPutOperation {
+
+  public static void main(String[] args) {
+
+    Map<String, String> map = new TreeMap<>();
+    map.put("test key 1", "test value 1");
+    map.put("test key 2", "test value 2");
+    map.put("test key 3", "test value 3");
+
+    System.out.println(map.put("test key 3", "test value 3"));
+    System.out.println(map.put("test key 4", "test value 4"));
+  }
+}
